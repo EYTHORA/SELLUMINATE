12fe51a7e721ad228498c2a9547c91e32576225a
HELDA LAURA <bajuitem995@gmail.com>
2025-01-22 23:51:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,266 @@
+(function($) {
+
+    "use strict";
+
+    var searchPopup = function() {
+      // open search box
+      $('#header-nav').on('click', '.search-button', function(e) {
+        $('.search-popup').toggleClass('is-visible');
+      });
+
+      $('#header-nav').on('click', '.btn-close-search', function(e) {
+        $('.search-popup').toggleClass('is-visible');
+      });
+      
+      $(".search-popup-trigger").on("click", function(b) {
+          b.preventDefault();
+          $(".search-popup").addClass("is-visible"),
+          setTimeout(function() {
+              $(".search-popup").find("#search-popup").focus()
+          }, 350)
+      }),
+      $(".search-popup").on("click", function(b) {
+          ($(b.target).is(".search-popup-close") || $(b.target).is(".search-popup-close svg") || $(b.target).is(".search-popup-close path") || $(b.target).is(".search-popup")) && (b.preventDefault(),
+          $(this).removeClass("is-visible"))
+      }),
+      $(document).keyup(function(b) {
+          "27" === b.which && $(".search-popup").removeClass("is-visible")
+      })
+    }
+
+    var countdownTimer = function() {
+      function getTimeRemaining(endtime) {
+        const total = Date.parse(endtime) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
+        const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(total / (1000 * 60 * 60 * 24));
+        return {
+          total,
+          days,
+          hours,
+          minutes,
+          seconds
+        };
+      }
+  
+      function initializeClock(id, endtime) {
+        const clock = document.getElementById(id);
+        const daysSpan = clock.querySelector('.days');
+        const hoursSpan = clock.querySelector('.hours');
+        const minutesSpan = clock.querySelector('.minutes');
+        const secondsSpan = clock.querySelector('.seconds');
+  
+        function updateClock() {
+          const t = getTimeRemaining(endtime);
+          daysSpan.innerHTML = t.days;
+          hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+          minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+          secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+          if (t.total <= 0) {
+            clearInterval(timeinterval);
+          }
+        }
+        updateClock();
+        const timeinterval = setInterval(updateClock, 1000);
+      }
+  
+      $('#countdown-clock').each(function(){
+        const deadline = new Date(Date.parse(new Date()) + 28 * 24 * 60 * 60 * 1000);
+        initializeClock('countdown-clock', deadline);
+      });
+    }
+
+    var initProductQty = function(){
+
+      $('.product-qty').each(function(){
+
+        var $el_product = $(this);
+        var quantity = 0;
+
+        $el_product.find('.quantity-right-plus').click(function(e){
+            e.preventDefault();
+            var quantity = parseInt($el_product.find('#quantity').val());
+            $el_product.find('#quantity').val(quantity + 1);
+        });
+
+        $el_product.find('.quantity-left-minus').click(function(e){
+            e.preventDefault();
+            var quantity = parseInt($el_product.find('#quantity').val());
+            if(quantity>0){
+              $el_product.find('#quantity').val(quantity - 1);
+            }
+        });
+
+      });
+
+    }
+
+    $(document).ready(function() {
+
+      searchPopup();
+      initProductQty();
+      countdownTimer();
+
+      /* Video */
+      var $videoSrc;  
+        $('.play-btn').click(function() {
+          $videoSrc = $(this).data( "src" );
+        });
+
+        $('#myModal').on('shown.bs.modal', function (e) {
+
+        $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+      })
+
+      $('#myModal').on('hide.bs.modal', function (e) {
+        $("#video").attr('src',$videoSrc); 
+      })
+
+      var mainSwiper = new Swiper(".main-swiper", {
+        speed: 500,
+        navigation: {
+          nextEl: ".main-slider-button-next",
+          prevEl: ".main-slider-button-prev",
+        },
+      });
+
+      var productSwiper = new Swiper(".product-swiper", {
+        spaceBetween: 20,        
+        navigation: {
+          nextEl: ".product-slider-button-next",
+          prevEl: ".product-slider-button-prev",
+        },
+        breakpoints: {
+          0: {
+            slidesPerView: 1,
+          },
+          660: {
+            slidesPerView: 3,
+          },
+          980: {
+            slidesPerView: 4,
+          },
+          1500: {
+            slidesPerView: 5,
+          }
+        },
+      });      
+
+      var testimonialSwiper = new Swiper(".testimonial-swiper", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        navigation: {
+          nextEl: ".testimonial-button-next",
+          prevEl: ".testimonial-button-prev",
+        },
+      });
+
+      var thumb_slider = new Swiper(".thumb-swiper", {
+        slidesPerView: 1,
+      });
+      var large_slider = new Swiper(".large-swiper", {
+        spaceBetween: 10,
+        effect: 'fade',
+        thumbs: {
+          swiper: thumb_slider,
+        },
+      });
+
+    }); // End of a document ready
+
+    window.addEventListener("load", function () {
+      const preloader = document.getElementById("preloader");
+      preloader.classList.add("hide-preloader");
+    });
+
+})(jQuery);
+
+ $('.wrap-slick3').each(function(){
+            $(this).find('.slick3').slick({
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                fade: true,
+                infinite: true,
+                autoplay: false,
+                autoplaySpeed: 6000,
+
+                arrows: true,
+                appendArrows: $(this).find('.wrap-slick3-arrows'),
+                prevArrow:'<button class="arrow-slick3 prev-slick3"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+                nextArrow:'<button class="arrow-slick3 next-slick3"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+
+                dots: true,
+                appendDots: $(this).find('.wrap-slick3-dots'),
+                dotsClass:'slick3-dots',
+                customPaging: function(slick, index) {
+                    var portrait = $(slick.$slides[index]).data('thumb');
+                    return '<img src=" ' + portrait + ' "/><div class="slick3-dot-overlay"></div>';
+                },  
+            });
+        });
+
+   // init Isotope
+    $(window).on('load', function () {
+        var $grid = $topeContainer.each(function () {
+            $(this).isotope({
+                itemSelector: '.isotope-item',
+                layoutMode: 'fitRows',
+                percentPosition: true,
+                animationEngine : 'best-available',
+                masonry: {
+                    columnWidth: '.isotope-item'
+                }
+            });
+        });
+    });
+    $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+      var nameTab = $(e.target).attr('href'); 
+      $(nameTab).find('.slick2').slick('reinit');          
+    });  
+    var Selector = {
+      DROPDOWN: '.dropdown',
+      NAV_LIST_GROUP: '.nav, .list-group',
+      ACTIVE: '.active',
+      DATA_TOGGLE: '[data-toggle="tab"], [data-toggle="pill"], [data-toggle="list"]',
+      DROPDOWN_TOGGLE: '.dropdown-toggle',
+      DROPDOWN_ACTIVE_CHILD: '> .dropdown-menu .active' };
+
+      $('.wrap-slick3').each(function(){
+        $(this).find('.slick3').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            infinite: true,
+            autoplay: false,
+            autoplaySpeed: 6000,
+
+            arrows: true,
+            appendArrows: $(this).find('.wrap-slick3-arrows'),
+            prevArrow:'<button class="arrow-slick3 prev-slick3"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+            nextArrow:'<button class="arrow-slick3 next-slick3"><i class="fa fa-angle-right" aria-hidden="true"></i></button>',
+
+            dots: true,
+            appendDots: $(this).find('.wrap-slick3-dots'),
+            dotsClass:'slick3-dots',
+            customPaging: function(slick, index) {
+                var portrait = $(slick.$slides[index]).data('thumb');
+                return '<img src=" ' + portrait + ' "/><div class="slick3-dot-overlay"></div>';
+            },  
+        });
+    });
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
